47258ef67fb948356f1515c1db7573f3477126b6
Alex <olivesargreek@gmail.com>
2021-08-07 08:48:35 +0300

    modified: src/index.js

--- src/index.js
+++ src/index.js
@@ -3,8 +3,74 @@ function eval() {
     return;
 }
 
-function expressionCalculator(expr) {
+const expressionCalculator = (str) => {
+
     // write your solution here
+    const operations = {
+        '+' : (a, b) => a + b,
+        '-' : (a, b) => a - b,
+        '*' : (a, b) => a * b,
+        '/' : (a, b) => {
+            if (b === 0) {
+                throw 'TypeError: Division by zero.';
+            }
+            return (a / b);
+        }
+    }
+    const operators = Object.keys(operations);
+
+    const processExpression = (expr) => {
+        for (let operator of operators) {
+            let regexPattern = `(?!^)(?<!\\${operators.map(el => '\\' + el).join('|')})\\${operator}`;
+            let found = [...expr.matchAll(new RegExp(regexPattern, 'g'))];
+            let matchedOperation = found[found.length - 1];
+
+            if (matchedOperation) {
+                let x = expr.slice(0, matchedOperation.index);
+                let y = expr.slice(matchedOperation.index + 1);
+        
+                return y ? operations[matchedOperation[0]](processExpression(x), processExpression(y)) : x;
+            }
+        } 
+       
+        return +expr;
+    }
+
+    const calculate = (str) => {
+
+        str = str.replace(/\s/g, '');
+
+        let stack = [];
+
+        for (let i = 0; i < str.length; i++) {
+            if (str[i] === ')') {
+                if (!stack.length) {
+                    throw 'ExpressionError: Brackets must be paired';
+                }
+
+                // expression in brackets, should be calculated
+                let toCalculate = str.slice(stack[stack.length - 1] + 1, i);
+
+                let calculated = processExpression(toCalculate);
+
+                // combine the whole expression again
+                str = str.slice(0, stack[stack.length - 1]) + calculated + str.slice(i + 1);
+
+                // return index back to the latest position of open bracket
+                i = stack.pop();
+
+            } else if (str[i] === '(') {
+                stack.push(i);
+            }
+        }
+        if (stack.length) {
+            throw 'ExpressionError: Brackets must be paired';
+        }
+        return processExpression(str);
+            
+    }
+    return calculate(str);
+   
 }
 
 module.exports = {
